68f6b970887fd9e1f31a5c88e1901ca4a1720275
DigitalCrafts <digitalcrafts@DigitalCraftss-MacBook-Air.local>
2018-11-19 14:24:16 -0500

    Classes

---
+++ human.py
@@ -0,0 +1,15 @@
+class human(object):
+	def __init__(self,name,power = 5):
+		self.name = name
+		self.health = 100
+		self.power = power
+thehuman = human('Link's)
+print thehuman.power
+print thehuman.name
+print thehuman.health
+
+# class Player(object):
+# 	species = "human"
+
+# puljos.Player()
+# puljos.species = "Robot"
